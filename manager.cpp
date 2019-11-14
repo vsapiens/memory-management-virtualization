a27fe31626bf67b4ec9abe9177126ca261ab2498
vsapiens <erick.frank@icloud.com>
2019-11-14 11:43:09 -0600

    Skeleton updated

---
+++ manager.cpp
@@ -0,0 +1,41 @@
+#include "ProcessManager.hpp"
+#include <iostream>
+/*
+manager.cc
+This program is the main that handles the process manager of memory and the output handler
+
+Erick Gonzalez A01039589
+Francisco Sánchez 
+Carlos Estrada
+Moisés Fernández
+Version: 1.0 last modified 14/11/2019
+*/
+
+int main()
+{
+
+char cOption;
+
+while (cOption != 'f')
+{
+    std::cout<<"Selecciona una opción del menú";
+    std::cin>> cOption;
+    switch (cOption)
+    {
+    case 'C':
+    case 'c':
+        
+        break;
+    case 'F':
+    case 'f':
+        
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+return 0;
+}
