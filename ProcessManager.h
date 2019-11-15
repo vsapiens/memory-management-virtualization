e17d56e817c5331aff35277b57d58cf45dde3146
Paco <paco.sanchez.ram@gmail.com>
2019-11-15 15:29:37 -0600

    refactored

---
+++ ProcessManager.h
@@ -0,0 +1,9 @@
+// ProcessManager.cpp
+// The header handles all the input phases of the program
+// from the name of the file to the input of the file.
+// Authors:
+// Erick Gonzalez A01039589
+// Francisco Sánchez A01196903
+// Carlos Estrada
+// Moisés Fernández
+// Version: 1.0 last modified 14/11/2019
