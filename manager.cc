095d0ddecd85af90a250777f4d3f5245cfdfe8ae
Carlos Estrada <carlos.deg02@hotmail.com>
2019-11-20 20:54:52 -0600

    Refactoring main

--- manager.cc
+++ manager.cc
@@ -1,12 +1,12 @@
-#include "ProcessManager.h"
 #include <iostream>
+#include "ProcessManager.h"
 
-// manager.cpp
+// manager.cc
 // This program is the main that handles the process manager of memory and the output handler
 
 // Erick Gonzalez A01039589
 // Francisco Sánchez A01196903
-// Carlos Estrada
+// Carlos Estrada A01039919
 // Moisés Fernández
 // Version: 1.0 last modified 14/11/2019
 
@@ -16,24 +16,6 @@
 // Return: Int, describing the correct compilation of the program
 int main()
 {
-    char cOption;
-
-    while (cOption != 'f')
-    {
-        std::cout << "Selecciona una opción del menú";
-        std::cin >> cOption;
-        switch (cOption)
-        {
-        case 'C':
-        case 'c':
-            break;
-        case 'F':
-        case 'f':
-            break;
-        default:
-            break;
-        }
-    }
 
     return 0;
 }
