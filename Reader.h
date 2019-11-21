ef35d28e5ab567c172525a08d5e4ca83d66ea94c
Carlos Estrada <carlos.deg02@hotmail.com>
2019-11-20 20:48:12 -0600

    Added read file

--- Reader.h
+++ Reader.h
@@ -1,18 +1,25 @@
 #include <vector>
 #include <string>
-
-
+#include <fstream>
 
 namespace sisops{
 
 namespace {
 
-
-
 }
 
-std::vector<std::string> readFile(){
+std::vector<std::string> readFile(std::string file){
+    std::ifstream input;
+    input.open("file");
 
+    std::vector<std::string> ins;
+
+    std::string buffer;
+    while (getline(input, buffer)) {
+        ins.push_back(buffer);
+    }
+
+    return ins;
 }
 
 }
